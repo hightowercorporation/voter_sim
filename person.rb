f3d5a1f25105d80e2e0a840b74d082700e92e0de
david hightower <w.david.hightower@gmail.com>
2017-10-13 23:50:15 +0000

    commit

---
+++ person.rb
@@ -0,0 +1,30 @@
+require '/.person'
+
+class Person
+	attr_reader :name
+	
+	def initialize()
+		
+	end
+		
+	def delete_person(person_name)
+				
+	end
+		
+	def create_person(person_name)		
+		person_name = Person.new
+	end
+		
+	def update_person
+	end
+		
+	def list_person
+	end
+	
+	def is_person_politician
+	end
+
+	def is_person_a_voter
+	end
+	
+end
---
+++ spec/person_spec.rb
@@ -0,0 +1,8 @@
+require './person'
+ 	
+describe Person do
+	it "can delete a person" do
+
+	end
+	
+end
