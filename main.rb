45d2e6c205028d68e400977a5e4f17e2951b483f
david hightower <w.david.hightower@gmail.com>
2017-11-04 20:44:49 +0000

    update

--- main.rb
+++ main.rb
@@ -14,6 +14,8 @@ class World
 			create
 		when "list", "l", "(l)ist"
 			puts @records.list
+		when "u", "update", "(u)pdate"
+			update
 		when "x", "exit", "e(x)it"
 			return "x"
 		end
@@ -69,6 +71,53 @@ class World
 		end
 	end	
   
+	def update
+		Questions.update_who
+		name = gets.chomp
+		index_for_voter = @records.search_voter(name)
+        if index_for_voter
+			Questions.update_name
+			new_name = gets.chomp
+			Questions.update_politics
+			new_politics = gets.chomp.downcase
+			case new_politics
+			when "l"
+				new_politics = "Liberal"
+				@records.update_voter(name,new_name, new_politics)
+			when "c"
+				new_politics = "Conservative"
+				@records.update_voter(name,new_name, new_politics)
+			when "t"
+				new_politics = "Tea Party"
+				@records.update_voter(name,new_name, new_politics)
+			when "s"
+				new_politics = "Socialist"
+				@records.update_voter(name,new_name, new_politics)
+			when "n"
+				new_politics = "Neutral"
+				@records.update_voter(name,new_name, new_politics)
+			else
+				Questions.invalid_entry
+			end
+		end
+		index_for_politician = @records.search_politician(name)
+		if index_for_politician
+			Questions.update_name
+			new_name = gets.chomp
+			Questions.update_party
+			new_party = gets.chomp.downcase
+			case party
+			when "d", "democrat", "(d)emocrat"
+				party = "Democrat"
+				@records.create_politician(name, party)
+			when "r", "republican", "(r)epublican"
+				party = "Republican"
+				@records.create_politician(name, party)
+			else
+				Questions.invalid_entry
+			end
+		end
+	end
 	world = World.new
 	Questions.welcome
 	while true 
